f58abdb0535e02bed58770fa6703581691e4ae00
andre souza <andre.souza@creathus.org.br>
2023-04-27 21:33:44 -0400

    chore: add a news testes in checkIn and functionalite of checkIn in different days

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,9 +1,6 @@
-import { expect, describe, it, beforeEach } from 'vitest'
-import { AuthenticateUseCase } from './authenticate'
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
-import { randomUUID } from 'node:crypto'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: CheckInUseCase
@@ -12,12 +9,50 @@ describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
     const {checkIn } = await sut.execute({
       userId: 'any_user_id',
       gymId: 'any_gym_id',
+    })
+
+    console.log(checkIn.created_at)
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8 ,0 , 0))
+
+    await sut.execute({
+      gymId: 'gym_01',
+      userId: 'user_01',
+    })
+
+    await expect(() => sut.execute({
+      gymId: 'gym_01',
+      userId: 'user_01',
+    })).rejects.toBeInstanceOf(Error)
+  })
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8 ,0 , 0))
+
+    await sut.execute({
+      gymId: 'gym_01',
+      userId: 'user_01',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8 ,0 , 0))
+
+    const {checkIn} = await sut.execute({
+      gymId: 'gym_01',
+      userId: 'user_01',
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
